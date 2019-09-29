d3e09d2e7d3519bf1c4b49110dd40b1524efef57
Kyle Luke <th.oldschool@gmail.com>
2019-09-29 03:18:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each {|student|
-      student.each {|key, value| self.send(("#{key}="), value)}
-      student
+      self.new(student)
     }
   end
 
