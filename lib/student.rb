e79ac43ddebfdaf8370fd351362571a04ddf20b2
Kyle Luke <th.oldschool@gmail.com>
2019-09-29 00:06:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
