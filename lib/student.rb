01bf2b19916ac2f246b5ff63702a5bef4403a847
Kyle Luke <th.oldschool@gmail.com>
2019-09-29 03:14:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each {|key, value| self.send(("#{key}="), value)}
+    students_array.each {|student| 
+      student.each {|key, value|
+        self.send(("#{key}="), value)
+      }
+    }
   end
 
   def add_student_attributes(attributes_hash)
