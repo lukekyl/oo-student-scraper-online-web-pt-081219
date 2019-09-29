b59505e2699e8df7448e5b6c9db062e1ccf25f9a
Kyle Luke <th.oldschool@gmail.com>
2019-09-29 00:13:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,11 @@ class Scraper
 
   def self.scrape_index_page(index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/")
     doc = Nokogiri::HTML(open(index_url))
-    doc.css("div.student-card").each {|student|
-      student = Student.new
-      student.name = student.css("h4.student-name").text
-      student.location = student.css("p.student-location").text
-      student.profile_url = student.css("a").href
+    doc.css("div.student-card").each {|person|
+      person = Student.new
+      person.name = student.css("h4.student-name").text
+      person.location = student.css("p.student-location").text
+      person.profile_url = student.css("a").href
     }
   end
 
