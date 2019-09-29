fe9fdcaaed099490a2f0c647027d6ad8c0ee18bb
Kyle Luke <th.oldschool@gmail.com>
2019-09-29 01:10:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
       student.name = person.css("h4.student-name").text
       student.location = person.css("p.student-location").text
       student.profile_url = person.css('a[href]')
-      p student
+      p "#{student.name} #{student.location} #{student.profile_url}"
     }
   end
 
